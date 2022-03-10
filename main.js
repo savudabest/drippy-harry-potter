a0501f1d50ef7b3ab2454998a202ba7bdc280618
savudabest <84152605+savudabest@users.noreply.github.com>
2022-03-10 17:43:46 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+	
+song = "";
+scoreLeftWrist = 0;
+ 
+function preload()
+{
+	song = loadSound("music.mp3");
+}
+ 
+rightWristX = 0;
+rightWristY = 0;
+ 
+leftWristX = 0;
+leftWristY = 0;
+ 
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+ 
+	video = createCapture(VIDEO);
+	video.hide();
+ 
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+ 
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+ 
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+	console.log(results);
+	scoreLeftWrist =  results[0].pose.keypoints[9].score;
+	console.log("scoreLeftWrist = " + scoreLeftWrist);
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+ 
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);	
+  }
+}
+ 
+function draw() {
+	image(video, 0, 0, 600, 500);
+    stroke("red");
+    fill("red");
+
+
+	circle(rightWristX, rightWristY, 20);
+	if(rightWristY > 0 && rightWristY <= 100){
+		document.getElementById("speed").innerHTML = "Speed = 0.5x";
+		song.rate(0.5);
+	}
+	else if(rightWristY > 100 && rightWristY <= 200){
+		document.getElementById("speed").innerHTML = "Speed = 1.0x";
+		song.rate(1);
+	}
+	else if(rightWristY > 200 && rightWristY <= 300){
+		document.getElementById("speed").innerHTML = "Speed = 1.5x";
+		song.rate(1.5);
+	}
+	else if(rightWristY > 300 && rightWristY <= 400){
+		document.getElementById("speed").innerHTML = "Speed = 2.0x";
+		song.rate(2);
+	}
+	else{
+		document.getElementById("speed").innerHTML = "Speed = 2.5x";
+		song.rate(2.5);
+	}
+	if(scoreLeftWrist > 0.2){
+    circle(leftWristX, leftWristY, 20);
+	InNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    volume = remove_decimals / 500;
+	document.getElementById("volume").innerHTML = "Volume = " + volume;	
+	}
+}
+ 
+function play()
+{
+	song.play();
+	song.setVolume(volume);
+	song.rate(1);
+}
